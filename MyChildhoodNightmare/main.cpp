c6bea5a7b294293f818359479a4989ac15d8bd8a
IvanAndreyshev-NOTEBOOK <vanyha36@yandex.ru>
2016-11-16 18:51:25 +0300

    First commit

---
+++ MyChildhoodNightmare/main.cpp
@@ -0,0 +1,14 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+
+static const int RESOLUTION_WIDTH = 1280;
+static const int RESOLUTION_HEIGHT = 720;
+
+int main()
+{
+	sf::RenderWindow window();
+	std::cout << "Hello, world!" "\n";
+
+	return 0;
+}
